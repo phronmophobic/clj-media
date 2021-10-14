912601f4dae3f5df66e368b950c16d5e242e87f5
Adrian Smith <smith.adriane@gmail.com>
2021-10-14 15:17:36 -0700

    initial commit.

---
+++ src/com/phronemophobic/clj_media.clj
@@ -0,0 +1,6 @@
+(ns com.phronemophobic.clj-media)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (prn x "Hello, World!"))
---
+++ test/com/phronemophobic/clj_media_test.clj
@@ -0,0 +1,7 @@
+(ns com.phronemophobic.clj-media-test
+  (:require [clojure.test :refer :all]
+            [com.phronemophobic.clj-media :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
