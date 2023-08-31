dff438b0b93054a4db33f4468671c982dd3a4474
Adrian Smith <smith.adriane@gmail.com>
2023-08-30 19:22:39 -0700

    Add more helpers for generating video frames with membrane.java2d.

--- src/com/phronemophobic/clj_media/java2d.clj
+++ src/com/phronemophobic/clj_media/java2d.clj
@@ -2,7 +2,6 @@
   (:require [membrane.ui :as ui]
             [membrane.java2d :as java2d]
             [clojure.java.io :as io]
-
             [com.phronemophobic.clj-media.av :as av]
             [com.phronemophobic.clj-media.video :as video]
             [com.phronemophobic.clj-media.av.raw :as raw
@@ -16,7 +15,10 @@
             [tech.v3.datatype :as dtype]
             [tech.v3.libs.buffered-image :as bufimg]
             )
-  (:import com.sun.jna.Pointer))
+  (:import com.sun.jna.Pointer
+           java.awt.Graphics2D
+           java.awt.Color
+           java.awt.RenderingHints))
 
 (raw/import-structs!)
 
@@ -76,5 +78,76 @@
       (catch Exception e
         (println e)))))
 
+
+
 (defn -main [fname]
   (play-video fname))
+
+(defn draw-to-image
+  ([elem size]
+
+   (let [[w h :as size] (if size
+                          size
+                          (let [[w h] (ui/bounds elem)
+                                [ox oy] (ui/origin elem)]
+                            [(+ w ox)
+                             (+ h oy)]))
+         _ (assert (and (pos? (first size))
+                        (pos? (second size)))
+                   "Size must be two positive numbers [w h]")
+         img (bufimg/new-image h w :byte-bgr)]
+     (binding [java2d/*g* (.createGraphics img)
+               java2d/*image-cache* (atom {})]
+       (.setRenderingHint ^Graphics2D java2d/*g*
+                          RenderingHints/KEY_ANTIALIASING
+                          RenderingHints/VALUE_ANTIALIAS_ON)
+       (.setRenderingHint ^Graphics2D java2d/*g*
+                          RenderingHints/KEY_TEXT_ANTIALIASING,
+                          RenderingHints/VALUE_TEXT_ANTIALIAS_ON)
+       (.setFont ^Graphics2D java2d/*g* (java2d/get-java-font (ui/font nil nil)))
+
+       (java2d/draw (ui/filled-rectangle [1 1 1]
+                                  w h))
+       (.setColor ^Graphics2D java2d/*g* (Color/BLACK))
+       (java2d/draw elem))
+     img)))
+
+(defn raster-data [img]
+  (let [raster (.getData img)
+        buffer (.getDataBuffer raster)
+        buf (.getData buffer)]
+    buf))
+
+(defn img->frame [img]
+  (let [w (.getWidth img)
+        h (.getHeight img)
+        pix-fmt AV_PIX_FMT_RGB24
+        frame (doto (av_frame_alloc)
+                (.writeField "width" w)
+                (.writeField "height" h)
+                (.writeField "format" pix-fmt))
+
+        err av_frame_get_buffer]
+    (assert (zero? (av_frame_get_buffer frame 0)))
+    (assert (zero? (av_frame_make_writable frame)))
+    (let [data-ptr (-> (.readField frame "data")
+                       first
+                       (.getPointer))
+          buf (raster-data img)
+          bytes-per-pixel 3
+          source-linesize (* bytes-per-pixel w)
+          dest-linesize (first (.readField frame "linesize"))]
+      (doseq [y (range h)]
+        (.write data-ptr (* y dest-linesize) buf (* y source-linesize) source-linesize)))
+
+    frame))
+
+(comment
+  (play-video "/Users/adrian/Downloads/231_sd_integrating_swiftui.mp4")
+  ;; "/Users/adrian/workspace/membrane/todo-app.mp4"
+
+  (java2d/draw-to-image)
+
+  ,)
+
+
