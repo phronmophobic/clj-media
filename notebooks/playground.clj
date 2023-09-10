4f9db2cbb72d555aa505242ee5d59c7f13f51618
Adrian Smith <smith.adriane@gmail.com>
2023-09-10 16:56:42 -0700

    add empty playground.

---
+++ notebooks/playground.clj
@@ -0,0 +1,13 @@
+(ns playground
+  (:require [nextjournal.clerk :as clerk]
+            [nextjournal.clerk.viewer :as v]
+            [clojure.java.io :as io]
+            [com.phronemophobic.clj-media :as clj-media]
+
+            [com.phronemophobic.clj-media.impl.av :as av]
+            [com.phronemophobic.clj-media.avfilter :as avfilter]))
+
+
+(defn -main [])
+
+
