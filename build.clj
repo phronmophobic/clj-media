6036f665e5f80b3a2d73bde39eee3c63d20a6e3a
Adrian Smith <smith.adriane@gmail.com>
2023-09-10 17:58:06 -0700

    version 0.9.0-SNAPSHOT.

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
             [clojure.string :as str]))
 
 (def lib 'com.phronemophobic/clj-media)
-(def version "0.9.0")
+(def version "0.9.0-SNAPSHOT")
 
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
